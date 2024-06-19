c029e88be702e9a08704d8e51f1d6bb497b2bfd7
codewithnickkk <154869787+codewithnickkk@users.noreply.github.com>
2024-06-19 22:41:01 +0530

    Add files via upload

---
+++ lec-55 string/script.js
@@ -0,0 +1,35 @@
+console.log("hello this is a string!");
+let a = "kem cho";
+console.log(a);
+console.log(a.length);
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+
+let name = "nick";
+let friend = "nick";
+
+console.log("i am "+ name +" and this is my mate "+friend);
+console.log(`i am ${name} and this is my mate ${friend}`);
+
+//escape sequence
+
+let q="kamlesh_bhai"
+console.log(q.toLowerCase());
+console.log(q.toUpperCase());
+
+//slicing
+let c = "mitraa"
+console.log(c.slice(1,4));
+console.log(c.slice(3,5));
+console.log(c.slice(0,6));
+
+//concate
+let f ="edwankar";
+console.log(f.concat("om","chaman"));
+console.log(f.replace("ed","chaman"));
---
+++ lec-56 exercise 2 business name generator/script.js
@@ -0,0 +1,73 @@
+/* Adjective:
+    Crazy
+    Amazing
+    Fire
+    
+   Shop name:
+    Engine
+    Foods
+    Garments
+    
+   Another word:
+    Bros
+    Limited
+    Hub*/
+
+let a1 = "Crazy";
+let a2 = "Amazing";
+let a3 = "Fire";
+
+
+let s1 = "Engine";
+let s2 = "Foods";
+let s3 = "Garments";
+
+
+let w1 = "Bros";
+let w2 = "Limited";
+let w3 = "Hub";
+
+let a = Math.random();
+let s = Math.random();
+let w = Math.random();
+
+
+if(a >= 0.6)
+{
+    console.log(a1);
+}
+else if(a >= 0.3)
+{
+    console.log(a2);
+}
+else{
+    console.log(a3);
+}
+
+
+if(s >= 0.6)
+{
+    console.log(s1);
+}
+else if(s >= 0.3)
+{
+    console.log(s2);
+}
+else{
+    console.log(s3);
+}
+
+
+if(w >= 0.6)
+{
+    console.log(w1);
+}
+else if(w >= 0.3)
+{
+    console.log(w2);
+}
+else{
+    console.log(w3);
+}
+
+
