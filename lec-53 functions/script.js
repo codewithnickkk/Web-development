003c2fc73e3fa933ff286801bda4b0326a213bdb
codewithnickkk <154869787+codewithnickkk@users.noreply.github.com>
2024-06-19 22:41:23 +0530

    Add files via upload

---
+++ lec-53 functions/script.js
@@ -0,0 +1,37 @@
+/* function sum(a, b) {
+    console.log(a + b);
+}
+
+sum(3,4); */
+
+/* function print(name) {
+    console.log("i am " + name);
+}
+
+print("lauda"); */
+
+
+/* function sum(a, b) {
+    return a + b;
+}    
+
+result = sum(2,3);
+console.log("result is "+result);
+ */
+
+
+/* function sum(a,b,c=0) {
+    return a+b+c
+}
+
+console.log(sum(3,4));
+console.log(sum(6,3,4)); */
+
+
+const func1 = (x)=>{
+    console.log("i am a arrow function "+x);
+    
+}
+
+func1(54);
+func1(43);
---
+++ lec-54 faulty calculator/script.js
@@ -0,0 +1,9 @@
+let a = Math.random();
+let b = 12;
+let c = 12;
+if (a > 0.7) {
+    console.log(b*c);
+} else {
+    console.log(b+c);
+    
+}
